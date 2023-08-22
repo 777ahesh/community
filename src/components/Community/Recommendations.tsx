9731e679bd3b1d7415fe8fd28a9613c8f63529b8
Mahesh Gaikwad <91421427+777ahesh@users.noreply.github.com>
2023-08-22 21:28:06 +0530

    updated the community

--- src/components/Community/Recommendations.tsx
+++ src/components/Community/Recommendations.tsx
@@ -61,10 +61,10 @@ const Recommendations: React.FC = () => {
         height="70px"
         borderRadius="4px 4px 0px 0px"
         fontWeight={700}
-        bgImage="url(/images/redditPersonalHome.png)"
+        // bgImage="url(/images/redditPersonalHome.png)"
         backgroundSize="cover"
         bgGradient="linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75)),
-        url('images/redditPersonalHome.png')"
+        // url('images/redditPersonalHome.png')"
       >
         Top Communities
       </Flex>
